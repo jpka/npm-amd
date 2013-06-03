a1fae96debc2d51d2003afcb65c019608a45ac38
Juan Pablo Kaniefsky <jpkaniefsky@gmail.com>
2013-06-03 17:03:27 -0300

    bin script bug fixed

--- cmd.js
+++ cmd.js
@@ -4,7 +4,7 @@ process.stdout.write("{\n");
 
 require("./index")({})
   .on("data", function(data) {
-    process.stdout.write("  " + data.id + ": " + data.path + ",\n");
+    process.stdout.write("  " + data[0] + ": " + data[1] + ",\n");
   })
   .on("end", function() {
     process.stdout.write("}");
